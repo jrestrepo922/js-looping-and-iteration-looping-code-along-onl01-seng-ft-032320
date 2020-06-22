5779effb3501d2f79a2b5a7052060f11e5113e07
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-22 17:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function writeCards(array, name) {
   let newArray = [];
   for(let i = 0; i < array.length; i++){
-    newArray.push(``)
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
   }
+  return newArray;
 }
