1aa55f3ab540bb975c0c5ccb8d5e79158794fe85
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-22 16:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Code your solutions in this file
-function writeCards(array, name)
+function writeCards(array, name) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    console.log(``)
+  }
+}
