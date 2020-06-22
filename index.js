c9b75e74d5448f8f4c35567fc8027dfb974e88cc
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-22 17:08:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+  }
+  return newArray;
+}
+
+
+
+
+function countDown(num) {
+  let i = num
+  
+  while (num >= 0) {
+    console.log(num)
+    num -= 1;
+  }
+}
